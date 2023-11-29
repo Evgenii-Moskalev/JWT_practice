e68f6932f816f6e56e6acd8484983dc30af6de3d
Evgenii Moskalev <evgenii.moskalev.cohort2211@gmail.com>
2023-11-28 20:41:11 -0800

    Adding refreshToken

--- authServer.js
+++ authServer.js
@@ -7,11 +7,22 @@ const jwt = require('jsonwebtoken')
 
 app.use(express.json())
 
-let refreshTokens = []
+let refreshTokens = [] //mimics DB
 
 app.post('/token', (req, res) => {
     const refreshToken = req.body.token
     if (refreshToken == null) return res.sendStatus(401)
+    if (!refreshTokens.includes(refreshToken)) return res.status(403)
+    jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+        if (err) return res.sendStatus(403)
+        const accessToken = generateAccessToken({ name: user.name })
+        res.json({ accessToken: accessToken })
+    })
+})
+
+app.delete('/logout', (req, res) => {
+    refreshTokens = refreshTokens.filter(token => token !== req.body.token)
+    res.sendStatus(204)
 })
 
 // const posts = [
